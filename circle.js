410f8ea0563ab2baeabb51c109b78d2dce5f9335
abbasinfoicon <uid3@infoiconsoftware.com>
2023-05-10 17:54:34 +0530

    header

---
+++ circle.js
@@ -0,0 +1,7 @@
+const text = document.querySelector(".text p");
+text.innerHTML = text.innerText
+    .split("")
+    .map(
+        (char, i) => `<span style="transform:rotate(${i * 5.8}deg)">${char}</span>`
+    )
+    .join("");
