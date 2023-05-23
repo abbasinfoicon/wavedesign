a8cb635f29b864da82542adeb35f59892d29f13a
abbasinfoicon <uid3@infoiconsoftware.com>
2023-05-23 12:10:03 +0530

    adsf

--- assets/js/custom-tree.js
+++ assets/js/custom-tree.js
@@ -3,7 +3,7 @@ var aspect = window.innerWidth / window.innerHeight;
 var camera = new THREE.PerspectiveCamera( 75, aspect, 0.1, 1000 );
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+// document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshNormalMaterial();
