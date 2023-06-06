334add0f0f3b7a4810521bdeb0202e47f3fc6d88
abbasinfoicon <uid3@infoiconsoftware.com>
2023-06-06 15:02:36 +0530

    agency-parallax-effect

--- assets/js/custom-animation.js
+++ assets/js/custom-animation.js
@@ -1,9 +1,26 @@
 $(function () {
-    var controller = new ScrollMagic.Controller();
-    var containerScene = new ScrollMagic.Scene({
-      triggerElement: '.aboutSecvice_child', duration: '100%'
-    })
-      .setClassToggle('.leftNewbtn', 'active')
-    //   .addIndicators()
-      .addTo(controller);
+  var controller = new ScrollMagic.Controller();
+
+  var containerScene = new ScrollMagic.Scene({
+    triggerElement: '.aboutSecvice_child',
+    duration: '100%'
+  }).setClassToggle('.leftNewbtn', 'active').addTo(controller);
+
+  var animations = [
+    { selector: ".agencyPra-box1", y: -180 },
+    { selector: ".agencyPra-box2", y: -600 },
+    { selector: ".agencyPra-box3", y: -180 }
+  ];
+
+  animations.forEach(function (animation) {
+    $(".agencyPra").each(function () {
+      var tween = new TimelineMax().to($(this).find(animation.selector), 1, { y: animation.y, ease: Linear.easeNone });
+
+      new ScrollMagic.Scene({
+        triggerElement: this,
+        triggerHook: 0.5,
+        duration: "100%"
+      }).setTween(tween).addTo(controller).addIndicators();
+    });
   });
+});
---
+++ assets/js/parallax-effect.js
@@ -0,0 +1,48 @@
+var objScrollTop = 0;
+var scroll = 0;
+
+$(window).on('load', function () {
+    parallax(document.getElementById('level1'), -1.5, 0);
+    parallax(document.getElementById('level2'), -1, 0);
+    parallax(document.getElementById('level3'), -1.3, 0);
+});
+
+$('body').on('mousewheel DOMMouseScroll MozMousePixelScroll wheel', function (e) {
+
+    if (e.originalEvent.wheelDelta !== undefined) {
+        parallax(document.getElementById('level1'), -1.5, e.originalEvent.wheelDelta);
+        parallax(document.getElementById('level2'), -1, e.originalEvent.wheelDelta);
+        parallax(document.getElementById('level3'), -1.3, e.originalEvent.wheelDelta);
+
+    } else if (e.originalEvent.deltaY !== undefined) {
+        parallax(document.getElementById('level1'), -1.5, (e.originalEvent.deltaY * (-30)));
+        parallax(document.getElementById('level2'), -1, (e.originalEvent.deltaY * (-30)));
+        parallax(document.getElementById('level3'), -1.3, (e.originalEvent.deltaY * (-30)));
+    }
+});
+
+function parallax(target, layer, scrollinit) {
+    var layer_coeff = 10 / layer;
+    scroll = scroll + (parseInt(scrollinit));
+
+    // var y = ($(window).height() - target.offsetHeight) / 2 - (scroll - ($(window).height() / 2)) / layer_coeff;
+    var y = ($(window).height() / 2) - (scroll - ($(window).height() / 2)) / layer_coeff;
+    var scarto = target.offsetHeight - $(window).height();
+
+    // Add below gallery when nearing the end of the column
+    if ((y < 0) && (Math.abs(y) >= scarto)) append_gallery(target);
+
+    if ((parseInt(scrollinit) >= 0) && (Math.abs(scroll) < 6000)) {
+        scroll = scroll - 150;
+        return;
+    }
+
+    $(target).css('marginTop', y);
+}
+
+function append_gallery(column) {
+    var container = $(column).find('.agencyPra-box'),
+        items = container.children();
+
+    items.clone().appendTo(container);
+}
