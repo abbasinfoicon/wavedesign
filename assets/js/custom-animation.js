7b47a0e9c73a5d77d8d007265d95c39e3f52529f
abbasinfoicon <uid3@infoiconsoftware.com>
2023-06-06 17:47:42 +0530

    blog

--- assets/js/custom-animation.js
+++ assets/js/custom-animation.js
@@ -14,7 +14,7 @@ $(function () {
 
   animations.forEach(function (animation) {
     $(".agencyPra").each(function () {
-      var tween = new TimelineMax().to($(this).find(animation.selector), 1, { y: animation.y, ease: Linear.easeNone });
+      var tween = new TimelineMax().to($(this).find(animation.selector), 1, { y: animation.y, ease: Power0.easeInOut });
 
       new ScrollMagic.Scene({
         triggerElement: this,
@@ -23,4 +23,41 @@ $(function () {
       }).setTween(tween).addTo(controller);
     });
   });
+
+  $(".blogPage").each(function () {
+    new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.7,
+      duration: '80%'
+    })
+      .setTween($(this).find('.customWidth'), { maxWidth: '100%', ease: Linear.easeNone })
+      .addTo(controller); // Optional: .addIndicators() Adds indicators for debugging purposes
+  });
+
+  $(".blogPage-sec").each(function () {
+    new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0.7,
+      duration: '100%'
+    })
+      .setTween($(this).find('.img-style'), { maxWidth: '100%', ease: Linear.easeNone })
+      .addTo(controller); // Optional: .addIndicators() Adds indicators for debugging purposes
+  });
+
+
 });
+
+// $(".blogPage").each(function () {
+//   var tl = new TimelineMax();
+//   var child = $(this).find(".blogPage img");
+//   tl.to(child, 1, { y: -180, ease: Linear.easeNone });
+
+//   var scene = new ScrollMagic.Scene({
+//     triggerElement: '.blogPage',
+//     triggerHook: 0.5,
+//     duration: "100%"
+//   })
+//     .setTween(tl)
+//     .addIndicators()
+//     .addTo(controller);
+// });
