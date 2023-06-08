76ca3a3b7b91af11129e408ba9ced5470f4b717f
abbasinfoicon <uid3@infoiconsoftware.com>
2023-06-08 18:07:11 +0530

    lightmode-home

--- test.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  var controller = new ScrollMagic.Controller();
-  var containerScene = new ScrollMagic.Scene({
-    triggerElement: '#container', duration: '50%'
-  })
-    .setClassToggle('#block', 'modifier1')
-    .addIndicators()
-    .addTo(controller);
-});
